0cd62f22508733acffe51ea00ee91be662a2c105
Bogdan Zagreba <b.zagreba@gmail.com>
2022-08-06 14:14:17 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'input_page.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        scaffoldBackgroundColor: Color(0xFF0A0D22),
+      ),
+      home: InputPage(),
+    );
+  }
+}
